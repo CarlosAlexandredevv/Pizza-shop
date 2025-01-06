7d0673dbd29ea28aa023b9cf0acaeab725a711a2
Carlos Alexandre <alexfilho062005@gmail.com>
2025-01-06 19:21:02 -0300

    :lipstick: feat: sign-up page styles

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -6,6 +6,7 @@ import { toast } from 'sonner'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { Link } from 'react-router-dom'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -38,6 +39,12 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className='p-8'>
+      <Button variant='ghost' asChild className='absolute right-4 top-8'>  
+        <Link to='/sign-up'>
+          Novo estabelecimento
+        </Link>
+      </Button>
+    
         <div className='w-[350px] flex flex-col justify-center gap-6'>
 
            <div className='flex flex-col gap-2 text-center'>
