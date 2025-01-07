59c8a7797a5a9adc20996e7f284e628144004ef2
Carlos Alexandre <alexfilho062005@gmail.com>
2025-01-07 13:21:41 -0300

    :lipstick: feat: component style order details

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,15 +1,25 @@
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger  } from "@/components/ui/dialog";
 import { TableRow, TableCell } from "@/components/ui/table";
+
+
 import { Search, ArrowRight, X } from "lucide-react";
+import { OrderDetails } from "./order-details";
 
 export function OrderTableRow() {
   return (
-    <TableRow>
+            <TableRow>
                     <TableCell>
-                      <Button variant="outline" size="xs">
-                        <Search className="h-3 w-3" />
-                        <span className="sr-only">Detalhes do pedido</span>
-                      </Button>
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button variant="outline" size="xs">
+                            <Search className="h-3 w-3" />
+                            <span className="sr-only">Detalhes do pedido</span>
+                          </Button>
+                      </DialogTrigger>
+
+                      <OrderDetails/>
+                      </Dialog>
                     </TableCell>
                     <TableCell className="font-mono text-xs font-medium">
                       821e78f7asdhdf128h
@@ -18,12 +28,7 @@ export function OrderTableRow() {
                       há 15 minutos
                     </TableCell>
                     <TableCell>
-                      <div className="flex items-center gap-2">
-                        <span className="h-2 w-2 rounded-full bg-slate-400" />
-                        <span className="font-medium text-muted-foreground">
-                          Pendente
-                        </span>
-                      </div>
+                      
                     </TableCell>
                     <TableCell className="font-medium">
                       Carlos Alexandre Rodrigues
