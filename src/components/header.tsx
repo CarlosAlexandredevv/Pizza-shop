af13d09eb0bde520ff42bb49caf870f9c051a3a0
Carlos Alexandre <alexfilho062005@gmail.com>
2025-01-07 10:03:57 -0300

    :sparkles: feat: theme toggl e and account dropdownmenu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 import { NavLink } from './nav-link.tsx'
 import { Separator } from './ui/separator'
+import { ThemeToggle } from './theme/theme-toggle.tsx'
+import { AccountMenu } from './account-menu.tsx'
 export function Header() {
   return (
     <div className="border-b">
@@ -21,6 +23,11 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className='ml-auto flex items-center gap-2 '>
+          <ThemeToggle />
+          <AccountMenu/>
+        </div>
 
       </div>
     </div>
